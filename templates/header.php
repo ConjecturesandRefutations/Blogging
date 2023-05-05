2e679b8380cb890914805984860cabe8daf2952c
Alfie Collins <alfiecollins44@gmail.com>
2023-05-05 13:37:07 +0100

    initial changes

---
+++ templates/header.php
@@ -0,0 +1,25 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Spot</title>
+    <!-- Materialize CSS linked below -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type='text/css'>
+        .brand{
+            background: #cbb09c !important;
+        }
+        .brand-text{
+            color: #cbb09c !important;
+        }
+    </style>
+</head>
+<body class="grey lighten-4">
+<nav class="white z-depth-0">
+    <div class="container">
+        <a href="#" class="brand-logo brand-text">Blog Spot</a>
+        <ul id='nav-mobile' class='right hide-on-small-and-down'>
+            <li><a href="#" class='btn brand z-depth-0'>Write a Blog</a></li>
+        </ul>
+    </div>
+</nav>
